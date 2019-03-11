52cabb168549d1b23f1504c46968d8c1528664e6
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2019-03-11 12:45:55 +0100

    Add create-runtime-image

--- java-modules/java-module-test/src/main/java/it/redhat/demo/test/WrapperInAction.java
+++ java-modules/java-module-test/src/main/java/it/redhat/demo/test/WrapperInAction.java
@@ -5,7 +5,7 @@ import it.redhat.demo.all.AllWrapper;
 public class WrapperInAction {
 
 	public static void main(final String[] args) {
-		String myText = "Inside text";
+		String myText = ( args.length == 0 ) ? "Inside text" : args[0];
 		AllWrapper wrapper = new AllWrapper( myText );
 
 		System.out.println( wrapper.getNameOne() );
