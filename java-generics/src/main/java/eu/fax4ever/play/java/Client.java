560ee1fcd604e8781d48ec2f697007d44845a55e
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2019-06-18 13:59:31 +0200

    Add generic test

--- java-generics/src/main/java/eu/fax4ever/play/java/Client.java
+++ java-generics/src/main/java/eu/fax4ever/play/java/Client.java
@@ -35,6 +35,27 @@ public class Client {
 		areEqual( alfa, new Wrapper<>( beta ) );
 	}
 
+	public void differentListsAtDifferentLevels() {
+		List<? extends Animal> subAnimals = new ArrayList<>();
+
+		List<Animal> animals = new ArrayList<>();
+		List<Person> people = new ArrayList<>();
+		List<Employee> employees = new ArrayList<>();
+
+		// subAnimals is more generic than animals
+		subAnimals = animals;
+		// NO animals = subAnimals;
+
+		// even is potentially it can contains the same instance
+		// can be assigned to a different level actual lists
+
+		subAnimals = people;
+		subAnimals = employees;
+
+		// NO animals = people;
+		// NO animals = employees;
+	}
+
 	private <T> boolean areEqual(T alfa, T beta) {
 		return alfa.equals( beta );
 	}
