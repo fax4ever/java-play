b6f3844f30fd505679ab305b3508946737bd308f
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2025-03-22 10:45:29 +0100

    Play with exercises

---
+++ simple/src/main/java/MergeSortedArray.java
@@ -0,0 +1,30 @@
+public class MergeSortedArray {
+
+   public void merge(int[] nums1, int m, int[] nums2, int n) {
+      int[] merge = new int[n+m];
+      int i = 0;
+      int j = 0;
+
+      for (int k=0; k<m+n; k++) {
+         if (i == m) {
+            // getting from num2
+            merge[k] = nums2[j++];
+         } else if (j == n) {
+            // getting from num1
+            merge[k] = nums1[i++];
+         } else if (nums1[i] < nums2[j]) {
+            // getting from num1
+            merge[k] = nums1[i++];
+         } else {
+            // getting from num2
+            merge[k] = nums2[j++];
+         }
+      }
+
+      // now merge contains our result
+
+      for (int k=0; k<m+n; k++) {
+         nums1[k] = merge[k];
+      }
+   }
+}
---
+++ simple/src/test/java/MergeSortedArrayTest.java
@@ -0,0 +1,19 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class MergeSortedArrayTest {
+
+   private final MergeSortedArray testSubject = new MergeSortedArray();
+
+   @Test
+   public void merge() {
+      int[] num1 = {1,2,3,0,0,0};
+      int[] num2 = {2,5,6};
+      int[] expected = {1,2,2,3,5,6};
+
+      testSubject.merge(num1, 3, num2, 3);
+      assertThat(num1).isEqualTo(expected);
+   }
+
+}
